79d224fd6149f8dbe6d903c21ffe926719d33495
Lanways <mcbluedd@gmail.com>
2023-02-25 21:50:39 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
+//設定連線到 Mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mogodb connected!')
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello world~~~!!')
 })
 
 app.listen(3000, () => {
