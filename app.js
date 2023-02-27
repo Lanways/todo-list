99ca6cdfe648304da371032c34114425cb1cb620
Lanways <mcbluedd@gmail.com>
2023-02-27 23:58:03 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Todo = require('./models/todo')
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -23,12 +24,29 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   // 拿到全部的Todo 資料
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name          // 從 req.body 拿出表單裡的 name 資料
+  // const todo = new Todo({ name })  // 存入資料庫            
+  // return todo.save()
+  //   .then(() => res.redirect('/')) // 新增完成後導回首頁
+  //   .catch(error => console.log(error))
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
