ede1976e350e4b600f8d52c98d57101ff1dc0625
Lanways <mcbluedd@gmail.com>
2023-03-02 23:19:45 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
   // 拿到全部的Todo 資料
   Todo.find()
     .lean()
+    .sort({ name: 'asc' }) // 反序 desc 也可用Mogodb自動生成的_id
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
